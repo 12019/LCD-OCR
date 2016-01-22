7cc740b81aee26351516b3803d4ab6666ee48634
php5engineer <php5engineer@gmail.com>
2016-01-22 18:24:55 +0300

    Added generated files

--- __init__.py
+++ __init__.py
@@ -1,9 +1,13 @@
-#
-# files = sorted(glob.glob('./img/[0-9][0-9].jpg'))
+import glob
+from visible.photo import Photo
+
+
+files = sorted(glob.glob('img/[0-9][0-9].jpg'))
 # files = ["./img/22.jpg"]
-# print files
-# for f in files[:]:
-#     try:
-#         LCDFinder(f)
-#     except AssertionError as e:
-#         print e
+print files
+for f in files[:]:
+    try:
+        p = Photo(f)
+        print p.extract_areas_with_digits(True, True)
+    except AssertionError as e:
+        print e
