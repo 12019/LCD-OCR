dd5501283519058f060fdb88fe540a1d1082ebdf
php5engineer <php5engineer@gmail.com>
2016-01-26 10:42:16 +0300

    video stub

---
+++ visible/video.py
@@ -0,0 +1,15 @@
+# import numpy as np
+# import cv2
+# import os
+#
+# path = 'low_light.mp4'
+# if not os.path.isfile(path):
+#     raise Exception('not a valid file')
+#
+# cap = cv2.VideoCapture(path)
+# while cap.isOpened():
+#     ret, frame = cap.read()
+#     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     cv2.imshow('frame', frame)
+
+# todo repair installation
