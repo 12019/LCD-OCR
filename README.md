cea322f8aa6a8ae67947e9267015e73a75959bf9
php5engineer <php5engineer@gmail.com>
2016-03-02 19:13:32 +0300

    trying to pass travis tests

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/php5engineer/LCD-OCR.svg?branch=master)](https://travis-ci.org/php5engineer/LCD-OCR)
+
 === LCD OCR ===
 
 Finds LCD display on a digital photo and prepares it's fragments for OCR.  
--- projection/adaptive.py
+++ projection/adaptive.py
@@ -11,13 +11,13 @@ class AdaptiveProjection(Projection):
     >>> # horizontal
     >>> bins = AdaptiveProjection(arr, Projection.TYPE_HORIZONTAL)
     >>> bins.get_projection()[3]
-    0.0019342359767891683
+    0.0019193857965451055
     >>> # bins.debug("adaptive horizontal")
     >>>
     >>> # vertical
     >>> bins = AdaptiveProjection(arr, Projection.TYPE_VERTICAL)
     >>> bins.get_projection()[3]
-    0.00097560975609756097
+    0.0009727626459143969
     >>> # bins.debug("adaptive vertical")
     """
 
@@ -27,7 +27,7 @@ class AdaptiveProjection(Projection):
             255,
             cv2.ADAPTIVE_THRESH_MEAN_C,
             cv2.THRESH_BINARY_INV,
-            self.get_stroke_width(1) * 2,
+            Projection.to_odd(self.get_stroke_width(1) * 2),
             5)
         # th, binary = cv2.threshold(self.image, 127, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
         self.latest_reference_image = binary
